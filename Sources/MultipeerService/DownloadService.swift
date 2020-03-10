dfa62603e76f3d46a1b729dbbb6f6d5a27f9fe93
Vikash Hart <vikashhart@gmail.com>
2020-03-10 19:45:22 -0400

    move test location

--- Sources/MultipeerService/DownloadService.swift
+++ Sources/MultipeerService/DownloadService.swift
@@ -2,12 +2,12 @@ import Foundation
 import MultipeerConnectivity
 import UIKit
 
-protocol DownloadServiceDelegate: class {
+public protocol DownloadServiceDelegate: class {
     func imageRecieved(image: UIImage, peer: String)
     func browserVCDismiss()
 }
 
-class DownloadService : NSObject {
+public class DownloadService : NSObject {
 
     // Service type must be a unique string, at most 15 characters long
     // and can contain only ASCII lowercase letters, numbers and hyphens.
@@ -20,15 +20,15 @@ class DownloadService : NSObject {
     private let serviceAdvertiser : MCNearbyServiceAdvertiser
     private let serviceBrowser : MCNearbyServiceBrowser
 
-    weak var delegate : DownloadServiceDelegate?
+    public weak var delegate : DownloadServiceDelegate?
 
-    lazy var session : MCSession = {
+    public lazy var session : MCSession = {
         let session = MCSession(peer: self.myPeerId, securityIdentity: nil, encryptionPreference: .required)
         session.delegate = self
         return session
     }()
 
-    override init() {
+    override public init() {
         self.serviceAdvertiser = MCNearbyServiceAdvertiser(peer: myPeerId, discoveryInfo: nil, serviceType: downloadServiceType)
         self.serviceBrowser = MCNearbyServiceBrowser(peer: myPeerId, serviceType: downloadServiceType)
 
@@ -44,24 +44,24 @@ class DownloadService : NSObject {
         self.serviceBrowser.stopBrowsingForPeers()
     }
 
-    func getBrowser() -> MCBrowserViewController {
+    public func getBrowser() -> MCBrowserViewController {
         let mcBrowser = MCBrowserViewController(serviceType: downloadServiceType, session: session)
         mcBrowser.delegate = self
         return mcBrowser
     }
 
-    func disconnectFromSession() {
+    public func disconnectFromSession() {
         session.disconnect()
     }
 }
 
 extension DownloadService : MCNearbyServiceAdvertiserDelegate {
 
-    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
+    public func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
         NSLog("%@", "didNotStartAdvertisingPeer: \(error)")
     }
 
-    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+    public func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
         NSLog("%@", "didReceiveInvitationFromPeer \(peerID)")
 //        invitationHandler(true, self.session)
     }
@@ -70,38 +70,38 @@ extension DownloadService : MCNearbyServiceAdvertiserDelegate {
 
 extension DownloadService : MCNearbyServiceBrowserDelegate {
 
-    func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
+    public func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
         NSLog("%@", "didNotStartBrowsingForPeers: \(error)")
     }
 
-    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
+    public func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
         NSLog("%@", "foundPeer: \(peerID)")
         NSLog("%@", "invitePeer: \(peerID)")
         browser.invitePeer(peerID, to: self.session, withContext: nil, timeout: 10)
     }
 
-    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+    public func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
         NSLog("%@", "lostPeer: \(peerID)")
     }
 }
 
 extension DownloadService: MCBrowserViewControllerDelegate {
-    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+    public func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
         delegate?.browserVCDismiss()
     }
 
-    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+    public func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
         delegate?.browserVCDismiss()
     }
 }
 
 extension DownloadService : MCSessionDelegate {
 
-    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+    public func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
         NSLog("%@", "peer \(peerID) didChangeState: \(state.rawValue)")
     }
 
-    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+    public func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
         NSLog("%@", "didReceiveData: \(data)")
         DispatchQueue.main.async {
             guard let image = UIImage(data: data) else { return }
@@ -109,15 +109,15 @@ extension DownloadService : MCSessionDelegate {
         }
     }
 
-    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+    public func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
         NSLog("%@", "didReceiveStream")
     }
 
-    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+    public func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
         NSLog("%@", "didStartReceivingResourceWithName")
     }
 
-    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+    public func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
         NSLog("%@", "didFinishReceivingResourceWithName")
     }
 }
---
+++ Tests/MultipeerServiceTests/TestSample.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vikash Hart on 3/10/20.
+//
+
+import MultipeerService
+import XCTest
+
+class TestFile: XCTestCase {
+    override class func setUp() {
+        print("running")
+    }
+    
+    override func tearDown() {
+        print("tearing down")
+    }
+    
+    func testSample() {
+        print("sample")
+    }
+}
+
+
+
